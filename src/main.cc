566b777ff4a6aafdd6d653e2be1f151e1a1052ee
ParrotPotato <niteshmeena698@gmail.com>
2024-08-03 16:39:56 +0900

    update: DearIMGUI working

--- src/main.cc
+++ src/main.cc
@@ -4,23 +4,102 @@
 
 #include <assert.h>
 
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_opengl3.h>
+
+void opengl_debug_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam){
+    // Ignore non-significant error/warning codes
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
+
+    std::string sourceStr;
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:             sourceStr = "API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   sourceStr = "Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: sourceStr = "Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     sourceStr = "Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     sourceStr = "Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:           sourceStr = "Other"; break;
+    }
+
+    std::string typeStr;
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:               typeStr = "Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: typeStr = "Deprecated Behavior"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  typeStr = "Undefined Behavior"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:         typeStr = "Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         typeStr = "Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              typeStr = "Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          typeStr = "Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           typeStr = "Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               typeStr = "Other"; break;
+    }
+
+    std::string severityStr;
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:         severityStr = "High"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       severityStr = "Medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          severityStr = "Low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: severityStr = "Notification"; break;
+    }
+
+    fprintf(stderr, "GL Debug: Source: %s, Type: %s, ID: %d, Severity: %s, Message: %s\n",
+        sourceStr.c_str(), typeStr.c_str(), id, severityStr.c_str(), message);
+}
+
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
-    auto windowHandle = SDL_CreateWindow("main window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 100, 100, SDL_WINDOW_OPENGL);
+
+    // NOTE: there is not error handling here, please add this in the future
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+    const char * shader_preprocessor = "#version 400";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+
+    auto windowHandle = SDL_CreateWindow("main window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     assert(windowHandle != nullptr);
+
     auto contextHandle = SDL_GL_CreateContext(windowHandle);
     assert(contextHandle != nullptr);
+
+    glewExperimental = GL_TRUE;
     if(glewInit() != GLEW_OK){
         std::cout << "failed to create context\n";
         return 0;
     }
+
+    int opengl_context_flag = 0;
+    glGetIntegerv(GL_CONTEXT_FLAGS, &opengl_context_flag);
+    if (opengl_context_flag & GL_CONTEXT_FLAG_DEBUG_BIT){
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+        glDebugMessageCallback(opengl_debug_message_callback, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    }
+
+
+    // starting imgui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO & io = ImGui::GetIO(); (void)io;
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL2_InitForOpenGL(windowHandle, contextHandle);
+    ImGui_ImplOpenGL3_Init(shader_preprocessor);
+
+    bool bvalue = false;
+
     glClearColor(0.1, 0.2, 0.3, 1.0);
     glClearDepth(1.0f);
-    while(1){
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    while(1){
         SDL_Event event;
         while(SDL_PollEvent(&event)){
+            ImGui_ImplSDL2_ProcessEvent(&event);
             switch (event.type){
                 case SDL_QUIT:
                     std::cout << "termination signal received by window\n";
@@ -30,9 +109,26 @@ int main() {
             }
         }
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Main Internal window");
+        ImGui::Checkbox("Boolean property", &bvalue);
+        if (ImGui::Button("Reset property")){
+            bvalue = false;
+        }
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(windowHandle);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
-    std::cout << "Hello world!\n";
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
     return 0;
 }
