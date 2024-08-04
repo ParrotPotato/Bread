bfd482273affd6dbea489eb64a73c389b3860116
ParrotPotato <niteshmeena698@gmail.com>
2024-08-04 11:13:38 +0900

    adds keyboard and mouse event handling

--- src/main.cc
+++ src/main.cc
@@ -2,15 +2,131 @@
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
 
+#include <glm/glm.hpp>
+
 #include <assert.h>
 
 #include <imgui.h>
 #include <imgui_impl_sdl2.h>
 #include <imgui_impl_opengl3.h>
 
+// Todo add keyboard / mouse / joystick support
+// potentially multiple joystick support for couch co-op
+
+#define     VISIBLEKEYS     128
+#define     FUNCTIONALKEYS  256
+#define     KEYOFFSET       SDLK_CAPSLOCK
+#define     KEYCOUNT        (VISIBLEKEYS + FUNCTIONALKEYS)
+
+
+struct KeyboardState{
+    bool cur[KEYCOUNT];
+    bool pre[KEYCOUNT];
+};
+inline int get_key_offset(int keycode){
+    return ((keycode >= KEYOFFSET)? (keycode - KEYOFFSET + VISIBLEKEYS) : (keycode));
+}
+
+
+#define     BUTTONCOUNT     6
+struct MouseState{
+    char cur[BUTTONCOUNT];
+    char pre[BUTTONCOUNT];
+
+    float  xpos, ypos;
+    float  xrel, yrel;
+};
+
+
+#define STATE_QUIT_REQUESTED      1 << 0
+#define STATE_KEYBOARD_KEY_CHANGE_EVENT  1 << 1
+#define STATE_KEYBOARD_KEY_REPEATER_EVENT 1 << 2
+#define STATE_MOUSE_BUTTON_EVENT  1 << 3
+#define STATE_MOUSE_MOTION_EVENT  1 << 4
+
+struct SystemStateHandler {
+    SDL_Window * window_handle;
+    SDL_GLContext  context_handle;
+
+    KeyboardState key;
+    MouseState    mouse;
+
+    int central_state;
+} g_state = {};
+
+
+inline void set_quit_request(){
+    g_state.central_state |= STATE_QUIT_REQUESTED;
+}
+
+inline bool is_quit_requested(){
+    return (g_state.central_state & STATE_QUIT_REQUESTED);
+}
+
+inline bool keyboard_key_repeater_event() {
+    return g_state.central_state & STATE_KEYBOARD_KEY_REPEATER_EVENT;
+}
+
+inline bool keyboard_key_change_event(){
+    return g_state.central_state & STATE_KEYBOARD_KEY_CHANGE_EVENT;
+}
+inline bool mouse_button_event(){
+    return g_state.central_state & STATE_MOUSE_BUTTON_EVENT;
+}
+inline bool mouse_motion_event(){
+    return g_state.central_state & STATE_MOUSE_MOTION_EVENT;
+}
+
+inline bool is_key_down(int keycode){
+    keycode = get_key_offset(keycode);
+    return g_state.key.cur[keycode] == true;
+}
+
+inline bool is_key_up(int keycode){
+    keycode = get_key_offset(keycode);
+    return g_state.key.cur[keycode] == false;
+}
+
+inline bool is_key_pressed(int keycode){
+    keycode = get_key_offset(keycode);
+    return g_state.key.cur[keycode] == true && g_state.key.pre[keycode] == false; 
+}
+
+inline bool is_key_released(int keycode){
+    keycode = get_key_offset(keycode);
+    return g_state.key.cur[keycode] == false && g_state.key.pre[keycode] == true; 
+}
+
+
+inline bool is_button_down(int buttoncode){
+    return g_state.mouse.cur[buttoncode] == true;
+}
+inline bool is_button_up(int buttoncode){
+    return g_state.mouse.cur[buttoncode] == false;
+}
+inline bool is_button_pressed(int buttoncode){
+    return g_state.mouse.cur[buttoncode] == true && g_state.mouse.pre[buttoncode] == false; 
+}
+inline bool is_button_released(int buttoncode){
+    return g_state.mouse.cur[buttoncode] == false && g_state.mouse.pre[buttoncode] == true; 
+}
+
+inline glm::vec2 mouse_window_pos(){
+    return glm::vec2(g_state.mouse.xpos, g_state.mouse.ypos);
+}
+
+inline glm::vec2 mouse_window_motion(){
+    return glm::vec2(g_state.mouse.xrel, g_state.mouse.yrel);
+}
+
+
+// Tasks 
+// 1. Setting up SDL2 and OPENGL build with cmake 
+// 2. Setting up DearIMGUI build with cmake
+
 void opengl_debug_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam){
     // Ignore non-significant error/warning codes
-    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
 
     std::string sourceStr;
     switch (source) {
@@ -47,6 +163,10 @@ void opengl_debug_message_callback(GLenum source, GLenum type, GLuint id, GLenum
         sourceStr.c_str(), typeStr.c_str(), id, severityStr.c_str(), message);
 }
 
+void initialize_central_state(){
+}
+
+
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -81,13 +201,15 @@ int main() {
     }
 
 
+    g_state.window_handle = windowHandle;
+    g_state.context_handle = contextHandle;
+
     // starting imgui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO & io = ImGui::GetIO(); (void)io;
 
     ImGui::StyleColorsDark();
-
     ImGui_ImplSDL2_InitForOpenGL(windowHandle, contextHandle);
     ImGui_ImplOpenGL3_Init(shader_preprocessor);
 
@@ -96,22 +218,102 @@ int main() {
     glClearColor(0.1, 0.2, 0.3, 1.0);
     glClearDepth(1.0f);
 
-    while(1){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            switch (event.type){
+    while(is_quit_requested() == false){
+
+        // resettign the central event state variable
+        g_state.central_state &= ~ (STATE_QUIT_REQUESTED);
+        g_state.central_state &= ~ (STATE_KEYBOARD_KEY_CHANGE_EVENT);
+        g_state.central_state &= ~ (STATE_KEYBOARD_KEY_REPEATER_EVENT);
+        g_state.central_state &= ~ (STATE_MOUSE_BUTTON_EVENT);
+        g_state.central_state &= ~ (STATE_MOUSE_MOTION_EVENT);
+
+        // reset keyboard and mouse
+        for(int i = 0 ; i < KEYCOUNT ; i++){
+            g_state.key.pre[i] = g_state.key.cur[i];
+        }
+
+        for(int i = 0 ; i < BUTTONCOUNT ; i++){
+            g_state.mouse.pre[i] = g_state.mouse.cur[i];
+        }
+        g_state.mouse.xrel = g_state.mouse.yrel;
+
+        // Setting local key buffer which can be used to call 
+        // upon the callback
+
+        int keyboard_events[256];
+        int mouse_events[5];
+
+        SDL_Event sdl_event;
+        while(SDL_PollEvent(&sdl_event)){
+            ImGui_ImplSDL2_ProcessEvent(&sdl_event);
+
+            // custom state handlers for special elements
+
+            // event state updater
+            switch(sdl_event.type){
                 case SDL_QUIT:
-                    std::cout << "termination signal received by window\n";
-                    return 0;
+                    g_state.central_state |= STATE_QUIT_REQUESTED;
+                    break;
+                case SDL_KEYDOWN:
+                    if (g_state.key.cur[get_key_offset(sdl_event.key.keysym.sym)] == false) {
+                        g_state.central_state |= STATE_KEYBOARD_KEY_CHANGE_EVENT;
+                    }
+                    g_state.key.cur[get_key_offset(sdl_event.key.keysym.sym)] =  true;
+                    keyboard_events[get_key_offset(sdl_event.key.keysym.sym)] = 0x0 | 1 << (sizeof(bool) * 8) | true;
+                    g_state.central_state |= STATE_KEYBOARD_KEY_REPEATER_EVENT;
+                    break;
+                case SDL_KEYUP:
+                    if (g_state.key.cur[get_key_offset(sdl_event.key.keysym.sym)] == true) {
+                        g_state.central_state |= STATE_KEYBOARD_KEY_CHANGE_EVENT;
+                    }
+                    g_state.key.cur[get_key_offset(sdl_event.key.keysym.sym)] =  false;
+                    keyboard_events[get_key_offset(sdl_event.key.keysym.sym)] = 0x0 | 1 << (sizeof(bool) * 8)  | false;
+                    g_state.central_state |= STATE_KEYBOARD_KEY_REPEATER_EVENT;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    g_state.mouse.cur[sdl_event.button.button] =  true;
+                    mouse_events[sdl_event.button.button]  = 0x0 | 1 << (sizeof(bool) * 8) | true;
+                    g_state.central_state |= STATE_MOUSE_BUTTON_EVENT;
+                    break;
+                case SDL_MOUSEBUTTONUP:
+                    g_state.mouse.cur[sdl_event.button.button] =  false;
+                    mouse_events[sdl_event.button.button]  = 0x0 | 1 << (sizeof(bool) * 8) | false;
+                    g_state.central_state |= STATE_MOUSE_BUTTON_EVENT;
+                    break;
+                case SDL_MOUSEMOTION:
+                    g_state.mouse.xrel = sdl_event.motion.xrel;
+                    g_state.mouse.yrel = sdl_event.motion.yrel;
+                    g_state.mouse.xpos = sdl_event.motion.x;
+                    g_state.mouse.ypos = sdl_event.motion.y;
+                    g_state.central_state |= STATE_MOUSE_MOTION_EVENT;
+                    break;
+                    // handle more events here
                 default:
                     break;
             }
         }
 
+
+        if (keyboard_key_change_event()){
+            std::cout << "keyboard key change event detected" << std::endl;
+        }
+        if (mouse_button_event()){
+            std::cout << "mouse button event detected" << std::endl;
+        }
+        if (mouse_motion_event()){
+            std::cout << "mouse motion event detected" << std::endl;
+        }
+
+
+        // all the update code will happen here in the main game loop
+
+        // all the update code will end here
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
+
+        // all the rendering code will happen here in the main game loop 
 
         ImGui::Begin("Main Internal window");
         ImGui::Checkbox("Boolean property", &bvalue);
@@ -119,6 +321,8 @@ int main() {
             bvalue = false;
         }
         ImGui::End();
+
+        // this is where all the rendering code will end
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -129,6 +333,9 @@ int main() {
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(contextHandle);
+    SDL_DestroyWindow(windowHandle);
 
     return 0;
 }
